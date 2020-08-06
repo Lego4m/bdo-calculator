7468af320b108b670a979938ffc6583deb4aeaa1
Leonardo Maron <leo22.maron@hotmail.com>
2020-08-06 15:40:23 -0300

    Updates

---
+++ precache-manifest.1181b8e449b4352e999053423c845106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d94984b62eb7896cda65bc877924525",
+    "url": "/bdo-calculator/index.html"
+  },
+  {
+    "revision": "4e69c15cf372feac9a22",
+    "url": "/bdo-calculator/static/css/main.b83a4f9a.chunk.css"
+  },
+  {
+    "revision": "87921197508e3f13bc17",
+    "url": "/bdo-calculator/static/js/2.d47c7eb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bdo-calculator/static/js/2.d47c7eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e69c15cf372feac9a22",
+    "url": "/bdo-calculator/static/js/main.d9cd03cf.chunk.js"
+  },
+  {
+    "revision": "03f9b0ce428ab47adb39",
+    "url": "/bdo-calculator/static/js/runtime-main.793a3107.js"
+  }
+]);
